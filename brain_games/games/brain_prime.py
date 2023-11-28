a861a876cd2d6b7009b9e3e0d9938be7438e547b
Zent7 <mihd020@gmail.com>
2023-11-28 20:17:17 +0500

    22

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,4 +1,4 @@
-import random
+simport random
 
 MIN_NUMBER = 1
 MAX_NUMBER = 100
@@ -24,8 +24,8 @@ def brain_prime():
 
     while True:
         number = random.randint(MIN_NUMBER, MAX_NUMBER)
-        print("Answer 'yes' if the given number is prime. "
-              "Otherwise, answer 'no'.")
+        print('Answer "yes" if the given number is prime. '
+              'Otherwise, answer "no".')
         print(f"Question: {number}")
 
         user_answer = input("Your answer: ").lower()
